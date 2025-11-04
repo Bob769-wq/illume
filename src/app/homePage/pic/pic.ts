7fbae44e7f4de543f664db0cd4e423034ee98812
Bob <dash78231@gmail.com>
2025-11-04 19:58:07 +0800

    initial commit

--- src/app/homePage/pic/pic.ts
+++ src/app/homePage/pic/pic.ts
@@ -5,9 +5,13 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <h2 class="text-4xl font-bold px-6 py-2.5">客房照片</h2>
-    <div class="grid  media-screen-800:grid-cols-2 gap-4 px-6 py-8">
+    <div class="grid media-screen-800:grid-cols-2 gap-4 px-6 py-8">
       <div class="cursor-pointer">
-        <img src="/pic1.webp" alt="pic1" class="overflow-hidden rounded-xl" />
+        <img
+          src="/pic1.webp"
+          alt="pic1"
+          class="overflow-hidden w-full h-full object-cover rounded-xl"
+        />
         <p class="text-center text-base py-1">中庭光照</p>
       </div>
       <div class="overflow-hidden rounded-xl cursor-pointer">
--- src/app/nav1/nav-pic/nav-pic.ts
+++ src/app/nav1/nav-pic/nav-pic.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
     <h2 class="text-4xl font-bold px-6 py-2.5">客房照片</h2>
     <div class="grid  media-screen-800:grid-cols-2 gap-4 px-6 py-8">
       <div class="cursor-pointer">
-        <img src="/nav-pic1.webp" alt="pic1" class="overflow-hidden rounded-xl" />
+        <img
+          src="/nav-pic1.webp"
+          alt="pic1"
+          class="overflow-hidden w-full h-full object-cover rounded-xl"
+        />
         <p class="text-center text-base py-1">客房光照</p>
       </div>
       <div class="overflow-hidden rounded-xl cursor-pointer">
